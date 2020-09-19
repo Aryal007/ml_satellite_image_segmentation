d70634e00712956a4874b0cda3dbece0bb5c6b86
Aryal007 <bibekaryal7@gmail.com>
2020-09-19 08:49:09 -0600

    added prediction for L07

--- utils/estimators.py
+++ utils/estimators.py
@@ -243,7 +243,8 @@ class Classifier():
         loaded_model = pickle.load(open(self.savepath+estimator, 'rb'))
         np_tiff = tiff.read()
         np_tiff = np_tiff.transpose(1,2,0)
-        mask = np.mean(np_tiff, axis=2) == (256 or 0)
+        # Check if values == 0 or 256 or nan for everything but last 2 axis
+        mask = (np.mean(np_tiff[:,:,:-2], axis=2) == (256 or 0)) * (np.isnan(np.mean(np_tiff[:,:,:-2], axis=2)))
         height = np_tiff.shape[0]
         width = np_tiff.shape[1]
         np_tiff = np_tiff.reshape(-1, np_tiff.shape[2]).astype('float64')
